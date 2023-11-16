1928e68ae6f6caf1ccf64fc8b2189ba8c4ce14e5
Guidotss <guidoolguin@hotmail.com>
2023-11-16 08:54:03 -0300

    Terminando endpoint para renovar el jwt

--- src/presentation/auth/controllers.ts
+++ src/presentation/auth/controllers.ts
@@ -72,4 +72,34 @@ export class AuthController {
       return this.handleErrors(err, res);
     }
   };
+
+  public renew = async (req: Request, res: Response) => {
+    const token = req.header("Authorization")?.split("Bearer ")[1];
+    if (!token) {
+      return res
+        .header("Content-Type", "application/json")
+        .status(401)
+        .json({ ok: false, message: "Unauthorized" });
+    }
+    try {
+      const { id } = (await JwtAdapter.verify(token)) as { id: string };
+      const user = await this.userRepository.findUserById(id);
+      if (!user) {
+        return res
+          .header("Content-Type", "application/json")
+          .status(404)
+          .json({ ok: false, message: "User not found" });
+      }
+      const newToken = await JwtAdapter.sign({ id: user.id });
+      const { name, email } = user;
+      return res.header("Content-Type", "application/json").status(200).json({
+        ok: true,
+        message: "User renewed successfully",
+        token: newToken,
+        data: { name, email },
+      });
+    } catch (err) {
+      return this.handleErrors(err, res);
+    }
+  };
 }
--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -2,20 +2,17 @@ import { Router } from "express";
 import { AuthController } from "./controllers";
 import { UserDataSourceImpl } from "../../infraestructure/datasources/user/user.datasource.impl";
 import { UserRepositoryImpl } from "../../infraestructure/repositories/user.repository.impl";
-import { JwtAdapter } from "../../config/jwt-adapter";
-
 
 export class AuthRoutes {
   static get routes() {
-
     const router = Router();
-    const datasource = new UserDataSourceImpl(); 
+    const datasource = new UserDataSourceImpl();
     const userRepository = new UserRepositoryImpl(datasource);
     const authController = new AuthController(userRepository);
 
     router.post("/register", authController.register);
     router.post("/login", authController.login);
-    
+    router.get("/renew", authController.renew); 
 
     return router;
   }
